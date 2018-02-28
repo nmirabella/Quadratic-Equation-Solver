71b5ca8845ca98dd4e53eeef86c448385c365b49
nmirabella <nick.mirabella0@gmail.com>
2018-02-27 22:36:08 -0500

    Initial commit using code generated by Spring Initializr.

---
+++ src/main/java/com/github/nmirabella/quadradicsolver/quadradicsolver/QuadradicsolverApplication.java
@@ -0,0 +1,12 @@
+package com.github.nmirabella.quadradicsolver.quadradicsolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadradicsolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadradicsolverApplication.class, args);
+	}
+}
